3e2ad734bc0177fc2842fa6159a05c25c8dae1cd
PankratovRoman <80282114+PankratovRoman@users.noreply.github.com>
2021-03-10 16:06:15 +0300

    Update console_rewrite.py

--- console_rewrite.py
+++ console_rewrite.py
@@ -3,29 +3,17 @@ class Command:
         self.name = name
         self.params = params
 
-    def check_key_param(self, user_input): # на входе у нас массив из [ключ(команда), все остальное(параметры и значения параметров)]
-        split_command = user_input.split()  # разделяем введенный текст на части
-        user_input_command = split_command[0]  # закидываем в переменную command_key нулевой элемент масссива из предыдущейй строки, который есть ключ к справочнику
-        if user_input_command not in command_list:  # проверяем есть ли выбранный ранее ключ в словаре, если нет, то пишем об этом
-            print('Command "{}" is not in command list. Type "help" for help.'.format(user_input_command))
-            return True
-
-
-        """Участок кода в котором я сомневаюсь, но как по-другому я не придумал. Сначала я смотрю есть ли вообще параметры в словаре для данной команды, если нет, то пасс.
-        Далее, если "длина введенного списка параметров меньше длины списка параметров в словаре - (длина списка параметров в словаре - 1)", тогда говорим, что параметров недостаточно."""
-        len_param = len(command_list[user_input_command].params) - (len(command_list[user_input_command].params)-1)
-        if len(split_command[1:]) == len(command_list[user_input_command].params):
-            pass
-        elif len(split_command[1:]) < len_param:
-            print('Expected parameters: {}'.format(command_list[user_input_command].params)) # к тому же вот тут возвращается список, а пробежать по нему циклом корректно чет не получается
-        """Конец участка в котором я сомневаюсь"""
-
-        command_list_item = command_list[user_input_command]  # получаем экземпляр класса
+    def check_param(self, user_input): # на входе у нас массив из [ключ(команда), все остальное(параметры и значения параметров)]
+        if (len(split_command)-1) < len(self.params):
+            print('Expected parameters: {}'.format(self.params)) # к тому же вот тут возвращается список, а пробежать по нему циклом корректно чет не получается
+        command_list_item = command_list[user_input]  # получаем экземпляр класса
         command_param = split_command[1:]  # присваиваем в command_param массив в котором все, что после ключа во введенном тексте
         split_param = (i.split(':') for i in command_param)  # получаем массив [параметр, значение]
         for i in split_param:  # проверяем, есть ли параметр из массива splited_param в параметрах команды из справочника
             if i[0] not in command_list_item.params:
                 print('Parameter "{}" is not in parameter list. Type "help {}" for help.'.format(i[0], user_input_command))
+        return True
+
 
 class Units:
     def __init__(self, name):
@@ -41,11 +29,51 @@ command_list = {'start': Command('start', [units['engine'].name, units['injector
                 'velocity': Command('velocity', [])
                 }
 
+def list_to_str(input_command):
+    debug_split = input_command.split()
+    debug_unit = debug_split[1:]
+    for i in debug_unit:
+        for i in command_list[i].params:
+            return i
+
+def help_command(input_command):
+    '''Функция, которая выводит help к каждой из команд'''
+    help_command = input_command.split(' ')
+    help_unit = help_command[1]
+    if help_unit not in command_list:
+        print('Command "{}" is not in command list. Type "help" for help.'.format(help_unit))
+    elif help_unit == 'start':
+        print('"start" gives the command to start one of the modules. Expected parameters: {}'.format(command_list[help_unit].params))
+    elif help_unit == 'power':
+        print('"power" gives the command to increase or decrease engine power. Expected parameters: {}'.format(command_list[help_unit].params))
+    elif help_unit == 'velocity':
+        print('"velocity" gives the command to increase or decrease velocity. Expected parameters: {}'.format(command_list[help_unit].params))
+
+print('Welcome to Enterprise test console. Type "help" for help.')
+
 while True:
     input_command = input('Enter command: ') # вводим команду
+    split_command = input_command.split()  # разделяем введенный текст на части
+    user_input_command = split_command[0]  # закидываем в переменную command_key нулевой элемент масссива из предыдущейй строки, который есть ключ к справочнику
 
     if input_command == 'exit':  # сразу проверяем не хочет ли пользователь выйти
         print('Terminal broke')
         break
+    elif user_input_command == 'debug' and (len(split_command) > 1):
+        i = list_to_str(input_command)
+        print(i)
+        continue
+    elif user_input_command == 'help' and (len(split_command) > 1):
+        help_command(input_command)
+        continue
+    elif input_command == 'help':
+        com_to_str = [k for k in command_list.keys()]
+        print('Expected commands: {}'.format(com_to_str))
+        continue
+    elif user_input_command not in command_list:  # проверяем есть ли выбранный ранее ключ в словаре, если нет, то пишем об этом
+        print('Command "{}" is not in command list. Type "help" for help.'.format(user_input_command))
+        continue
 
-    Command.check_key_param(command_list, input_command)
+
+    Command.check_param(command_list[user_input_command], user_input_command)
+
